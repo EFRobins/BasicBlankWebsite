7c387248320bc5a1a3f4fd11953f5c2266034448
EFRobins <robinsfinley@gmail.com>
2023-08-15 20:48:41 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+const routes = require('./routes');
+app.set('view engine', 'ejs');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Listen on port 3000
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
+  
+module.exports = app;
+  
